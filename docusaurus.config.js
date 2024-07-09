89e5b2ca700d24dafe078ccfab4527f4cd0eb8b5
ArchFeh <archfeh@outlook.com>
2024-07-09 14:17:04 +0800

    Update website
    Signed-off-by: ArchFeh <archfeh@outlook.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: "https://ruyisdk.org/favicons/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://arhchfeh.github.io",
+  url: "https://archfeh.xyz",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/ruyisdk-org/",
